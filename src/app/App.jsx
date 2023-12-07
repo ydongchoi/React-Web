0fb21086b03fbd451b8e0367a89034a9c1a51efb
YD <105weapon@gmail.com>
2023-12-07 17:35:04 +0900

    feat(App): added keycloak-provider instead of auth provider

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,10 @@
+import * as React from 'react';
 import { CssBaseline } from '@mui/material';
 import { useRoutes } from 'react-router-dom';
 import { MatxTheme } from './components';
-import { AuthProvider } from './contexts/JWTAuthContext';
+import Keycloak from 'keycloak-js';
+import { ReactKeycloakProvider } from '@react-keycloak/web'
+import { config }from '../Constants'
 import { SettingsProvider } from './contexts/SettingsContext';
 import routes from './routes';
 import '../fake-db';
@@ -9,14 +12,22 @@ import '../fake-db';
 const App = () => {
   const content = useRoutes(routes);
 
+  const keycloak = new Keycloak({
+    url: `${config.url.KEYCLOAK_BASE_URL}`,
+    realm: "spring-boot-microservices-realm",
+    clientId: "react-test"
+  })
+
   return (
     <SettingsProvider>
-      <AuthProvider>
+      <ReactKeycloakProvider
+        authClient={keycloak}
+      >
         <MatxTheme>
           <CssBaseline />
           {content}
         </MatxTheme>
-      </AuthProvider>
+      </ReactKeycloakProvider>
     </SettingsProvider>
   );
 };
